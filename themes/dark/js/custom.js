bfc36ddff1b77f184014d222d60b059eeaa9d633
joanlafulla <joanlafulla@hotmail.com>
2025-01-14 18:44:22 +0100

    Hola mundo

--- themes/dark/js/custom.js
+++ themes/dark/js/custom.js
@@ -24,6 +24,8 @@ const highlighted = document.querySelector(".highlighted");
 const maskHighlighted = document.querySelector(".mask_highlighted");
 const videoHighlighted = document.querySelectorAll(".video_highlighted");
 
+console.log("Hola mundo");
+
 let mypage = '';
 
 function getmypage() {
