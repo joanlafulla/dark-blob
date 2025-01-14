e7ee19f8efb4a77850b7bd6d6af72d4d82adc156
joanlafulla <joanlafulla@hotmail.com>
2025-01-14 19:03:15 +0100

    Ver las diffs

--- themes/dark/js/custom.js
+++ themes/dark/js/custom.js
@@ -24,6 +24,7 @@ const highlighted = document.querySelector(".highlighted");
 const maskHighlighted = document.querySelector(".mask_highlighted");
 const videoHighlighted = document.querySelectorAll(".video_highlighted");
 
+console.log("Ver las iferencias")
 
 let mypage = '';
 
